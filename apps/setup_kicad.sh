ade425a207ba34bb3ab5f7a80658dad052c3723d
zst123 <zst123@users.noreply.github.com>
2020-08-29 21:27:22 +0800

    Kicad plugins

--- apps/setup_kicad.sh
+++ apps/setup_kicad.sh
@@ -1,3 +1,9 @@
 sudo add-apt-repository ppa:js-reynaud/kicad-5.1
 sudo apt update
 sudo apt install --install-recommends -y kicad kicad-demos
+
+mkdir -p $HOME/.kicad_plugins
+cd $HOME/.kicad_plugins
+
+git clone https://github.com/openscopeproject/InteractiveHtmlBom
+

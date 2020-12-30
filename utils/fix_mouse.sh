713b58ac30e20089cffb7c83c90a0f7f658f3651
zst123 <zst123@users.noreply.github.com>
2020-12-30 11:30:58 +0800

    Improve fix-mouse utility

--- utils/fix_mouse.sh
+++ utils/fix_mouse.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
+# Restart mouse driver
 sudo modprobe -r psmouse
 sudo modprobe psmouse
+
+# Restart natural scroll
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
+
+# Restart gestures
+gestures &
